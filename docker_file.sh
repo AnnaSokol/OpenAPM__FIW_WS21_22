9ee17e09ed01c2dc1700eb9e796980aaa97cf502
anea <sokolov-anna@outlook.de>
2021-12-07 01:37:36 +0100

    delete

--- docker_file.sh
+++ docker_file.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo snap install docker
-sudo install docker -y
-sudo service docker start
-sudo docker pull grafana/grafana
-sudo docker run -d --name=grafana -p 3000:3000 grafana/grafana
+#sudo apt-get update
+#sudo snap install docker
+#sudo install docker -y
+#sudo service docker start
+#sudo docker pull grafana/grafana
+#sudo docker run -d --name=grafana -p 3000:3000 grafana/grafana
